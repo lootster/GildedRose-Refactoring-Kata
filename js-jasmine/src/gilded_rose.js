cf910eb19b4f3bc2c46447408894122e8d4d06bb
Sheldon <sheldon.lim@online-pajak.com>
2019-03-25 12:37:26 +0800

    refactor

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -22,29 +22,30 @@ class Shop {
         if (this.items[i].name === BACKSTAGE_PASSES) {
           this.increaseBackStagePassesQuality(i);
         }
+        if (this.items[i].name === AGED_BRIE) this.increaseItemQuality(i);
         if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE_PASSES) {
-          if (this.items[i].quality > 0) this.decreaseItemQuality(i);
+          if (this.items[i].quality > 0) {
+            this.decreaseItemQuality(i);
+            // Decrease Quality another time if SellIn has expired
+            if (this.isSellInExpired(i)) this.decreaseItemQuality(i);
+          }
         } 
-        else {
-          this.increaseItemQuality(i);
-        }
+        // else {
+        //   this.increaseItemQuality(i);
+        // }
       }
 
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE) {
-          if (this.items[i].name != BACKSTAGE_PASSES) {
-            if (this.items[i].quality > 0) {
-              this.decreaseItemQuality(i);
-            }
-          } else {
+          if (this.items[i].name === BACKSTAGE_PASSES) {
             this.decreaseQualityToZero(i);
           }
-        } else {
-            this.increaseItemQuality(i);
-        }
       }
     }
     return this.items;
+  }
+
+  isSellInExpired(i) {
+    return this.items[i].sellIn < 0;
   }
 
   decreaseQualityToZero(i) {
@@ -61,12 +62,9 @@ class Shop {
 
   increaseBackStagePassesQuality(i) {
     if (this.items[i].name == BACKSTAGE_PASSES) {
-      if (this.items[i].sellIn < 11) {
-        this.increaseItemQuality(i);
-      }
-      if (this.items[i].sellIn < 6) {
-        this.increaseItemQuality(i);
-      }
+      this.increaseItemQuality(i);
+      if (this.items[i].sellIn < 11) this.increaseItemQuality(i);
+      if (this.items[i].sellIn < 6) this.increaseItemQuality(i);
     }
   }
 
