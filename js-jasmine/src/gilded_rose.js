446ea291dbb418f424c1559f0d3f31e3549dd869
Sheldon <sheldon.lim@online-pajak.com>
2019-03-25 10:32:02 +0800

    Refactor

--- js-jasmine/src/gilded_rose.js
+++ js-jasmine/src/gilded_rose.js
@@ -14,11 +14,12 @@ class Shop {
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
+
+      if (this.items[i].name === SULFURAS) return this.items;
+
       if (this.items[i].name != AGED_BRIE && this.items[i].name != BACKSTAGE_PASSES) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != SULFURAS) {
-            this.decreaseItemQuality(i);
-          }
+          this.decreaseItemQuality(i);
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -26,16 +27,14 @@ class Shop {
           this.increaseQualityForBackStagePasses(i);
         }
       }
-      if (this.items[i].name != SULFURAS) {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
+   
+      this.items[i].sellIn = this.items[i].sellIn - 1;
+      
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != AGED_BRIE) {
           if (this.items[i].name != BACKSTAGE_PASSES) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != SULFURAS) {
                 this.decreaseItemQuality(i);
-              }
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
